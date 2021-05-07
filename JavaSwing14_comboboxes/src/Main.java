04a239458531c0d9db1ebb849bb211242657fd60
nnthach173 <83439525+nnthach173@users.noreply.github.com>
2021-05-07 21:58:17 +0700

    1st commit

---
+++ JavaSwing14_comboboxes/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// JComboBox = a component that combines a button or editable field and a drop-down list
+		
+	new MyFrame();	
+	}
+
+}
